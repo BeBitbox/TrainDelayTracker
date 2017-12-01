c091055fcd23818b4a5db58f8d7a76ff9bc26d52
Bitbox <info@bitbox.be>
2017-12-01 08:21:13 +0100

    Springboot setup

---
+++ src/main/java/be/bitbox/traindelay/belgian/tracker/BelgianTrainDelayTrackerApplication.java
@@ -0,0 +1,12 @@
+package be.bitbox.traindelay.belgian.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelgianTrainDelayTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelgianTrainDelayTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/be/bitbox/traindelay/belgian/tracker/BelgianTrainDelayTracker/BelgianTrainDelayTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.bitbox.traindelay.belgian.tracker.BelgianTrainDelayTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BelgianTrainDelayTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
