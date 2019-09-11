42a59ccb2223580c1848f9e04d491c09bf0efa50
Franky Vertriest <Franky.Vertriest@tothepoint.company>
2019-09-11 23:14:08 +0200

    First step for internationalization

--- ui/src/main/java/be/bitbox/traindelay/tracker/ui/HomePageUI.java
+++ ui/src/main/java/be/bitbox/traindelay/tracker/ui/HomePageUI.java
@@ -1,13 +1,16 @@
 package be.bitbox.traindelay.tracker.ui;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.data.renderer.IconRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
+import com.vaadin.flow.function.SerializableFunction;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,21 +34,30 @@ public class HomePageUI extends VerticalLayout {
         var trainDeparturesTab = new Tab("Train Departures");
         tabOverview.put(overviewTab, homePageDivController::asDiv);
         tabOverview.put(trainDeparturesTab, trainDepartureDivController::asDiv);
-        
+
         var tabs = new Tabs(overviewTab, trainDeparturesTab);
         overviewTab.setSelected(true);
         tabs.addSelectedChangeListener(event -> setTabsVisibility());
         var titleH1 = new H1("TrainTraffic.be");
         titleH1.addClassName("titleH1");
         var subTitleH3 = new H3("Independent train departure statistics");
-        subTitleH3.addClassName("subTitleH3");
-        Header header = new Header(titleH1, subTitleH3);
+        var languagePicker = new ComboBox<LanguageOption>();
+        languagePicker.addClassName("languagePicker");
+        languagePicker.setRenderer(new IconRenderer<>(languageOption -> languageOption));
+
+        var optionDutch = new LanguageOption(new Image("frontend/nl_flag.png", "Nederlands"), "Nederlands");
+        var optionFrench = new LanguageOption(new Image("frontend/fr_flag.png", "Français"), "Français");
+        var optionEnglish = new LanguageOption(new Image("frontend/uk_flag.png", "English"), "English");
+        languagePicker.setItems(optionDutch, optionFrench, optionEnglish);
+        ComboBox comboBox = new ComboBox();
+
+        Header header = new Header(titleH1, subTitleH3, languagePicker);
         header.addClassName("header");
-        
+
         add(header, tabs);
         setTabsVisibility();
     }
-    
+
     private void setTabsVisibility() {
         for (Map.Entry<Tab, Supplier<Div>> entry : tabOverview.entrySet()) {
             var tab = entry.getKey();
@@ -61,6 +73,26 @@ public class HomePageUI extends VerticalLayout {
                 ofNullable(divs.get(tab))
                         .ifPresent(div -> div.setVisible(false));
             }
+        }
+    }
+
+    private static class LanguageOption extends Span {
+        private final Image image;
+        private final String language;
+
+        private LanguageOption(Image image, String language) {
+            this.image = image;
+            this.language = language;
+            add(image);
+            add(language);
+        }
+
+        public Image getImage() {
+            return image;
+        }
+
+        public String getLanguage() {
+            return language;
         }
     }
 }
